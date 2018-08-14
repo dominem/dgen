cb87838c23b6cdf03663e4d878124a5b5ff3ec54
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2018-08-14 15:49:39 +0200

    Refactor tempdir tests using unittest.TestCase

--- tests/test_tempdir.py
+++ tests/test_tempdir.py
@@ -1,14 +1,17 @@
 import os
 import shutil
+import unittest
 from dgen.tempdir import tempdir
 
 
-class TestMake(object):
+class TestMake(unittest.TestCase):
+    def tearDown(self):
+        tempdir.remove()
+
     def test_creates_empty_tempdir(self):
         tempdir.make()
         assert os.path.exists(tempdir.path)
         assert not os.listdir(tempdir.path)     # assert is-empty
-        tempdir.remove()
 
     def test_removes_existing_tempdir_with_its_content_and_creates_new_one(self):
         tempdir.make()
@@ -17,17 +20,22 @@ class TestMake(object):
 
         tempdir.make()
         assert not os.listdir(tempdir.path)     # assert is-empty
-        tempdir.remove()
 
     def test_removes_existing_file_that_has_name_of_tempdir_and_creates_tempdir(self):
         open(tempdir.dirname, 'w').close()
         assert os.path.isfile(tempdir.path)
         tempdir.make()
         assert os.path.isdir(tempdir.path)
+
+
+class TestCopyFiles(unittest.TestCase):
+    def setUp(self):
+        self.dst_dir = os.path.abspath('dst_dir')
+
+    def tearDown(self):
+        shutil.rmtree(self.dst_dir)
         tempdir.remove()
 
-
-class TestCopyFiles(object):
     def test_copies_files_from_tempdir_to_existing_dir(self):
         tempdir.make()
 
@@ -36,16 +44,12 @@ class TestCopyFiles(object):
         open(os.path.join(tempdir.path, file1), 'w').close()
         open(os.path.join(tempdir.path, file2), 'w').close()
 
-        dst_dir = os.path.abspath('dst_dir')
-        os.mkdir(dst_dir)
+        os.mkdir(self.dst_dir)
 
-        tempdir.copy_files([file1, file2], dst_dir)
+        tempdir.copy_files([file1, file2], self.dst_dir)
 
-        assert os.path.isfile(os.path.join(dst_dir, file1))
-        assert os.path.isfile(os.path.join(dst_dir, file2))
-
-        shutil.rmtree(dst_dir)
-        tempdir.remove()
+        assert os.path.isfile(os.path.join(self.dst_dir, file1))
+        assert os.path.isfile(os.path.join(self.dst_dir, file2))
 
     def test_copies_files_from_tempdir_to_not_existing_dir(self):
         tempdir.make()
@@ -55,15 +59,10 @@ class TestCopyFiles(object):
         open(os.path.join(tempdir.path, file1), 'w').close()
         open(os.path.join(tempdir.path, file2), 'w').close()
 
-        dst_dir = os.path.abspath('dst_dir')
+        tempdir.copy_files([file1, file2], self.dst_dir)
 
-        tempdir.copy_files([file1, file2], dst_dir)
-
-        assert os.path.isfile(os.path.join(dst_dir, file1))
-        assert os.path.isfile(os.path.join(dst_dir, file2))
-
-        shutil.rmtree(dst_dir)
-        tempdir.remove()
+        assert os.path.isfile(os.path.join(self.dst_dir, file1))
+        assert os.path.isfile(os.path.join(self.dst_dir, file2))
 
     def test_copies_files_from_tempdir_to_not_existing_path(self):
         tempdir.make()
@@ -73,13 +72,9 @@ class TestCopyFiles(object):
         open(os.path.join(tempdir.path, file1), 'w').close()
         open(os.path.join(tempdir.path, file2), 'w').close()
 
-        dst_dir_parent = os.path.abspath('dst_dir_parent')
-        dst_dir = os.path.join(dst_dir_parent, 'dst_dir')
+        dst_dir_child = os.path.join(self.dst_dir, 'dst_dir_child')
 
-        tempdir.copy_files([file1, file2], dst_dir)
+        tempdir.copy_files([file1, file2], dst_dir_child)
 
-        assert os.path.isfile(os.path.join(dst_dir, file1))
-        assert os.path.isfile(os.path.join(dst_dir, file2))
-
-        shutil.rmtree(dst_dir_parent)
-        tempdir.remove()
+        assert os.path.isfile(os.path.join(dst_dir_child, file1))
+        assert os.path.isfile(os.path.join(dst_dir_child, file2))
