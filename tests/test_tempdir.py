b6bbd731aa89a22a578465ddcea8a5927c1ce849
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2018-08-14 14:27:33 +0200

    Add more tests for tempdir

--- tests/test_tempdir.py
+++ tests/test_tempdir.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from dgen.tempdir import tempdir
 
 
@@ -25,4 +26,63 @@ def test_tempdir_make_removes_existing_temp_file_and_creates_new_dir():
     assert os.path.isfile(tempdir.path)
     tempdir.make()
     assert os.path.isdir(tempdir.path)
+    tempdir.remove()
+
+
+def test_tempdir_copy_files_copies_files_from_tempdir_to_existing_dir():
+    tempdir.make()
+
+    file1 = 'file1'
+    file2 = 'file2'
+    open(os.path.join(tempdir.path, file1), 'w').close()
+    open(os.path.join(tempdir.path, file2), 'w').close()
+
+    dst_dir = os.path.abspath('dst_dir')
+    os.mkdir(dst_dir)
+
+    tempdir.copy_files([file1, file2], dst_dir)
+
+    assert os.path.isfile(os.path.join(dst_dir, file1))
+    assert os.path.isfile(os.path.join(dst_dir, file2))
+
+    shutil.rmtree(dst_dir)
+    tempdir.remove()
+
+
+def test_tempdir_copy_files_copies_files_from_tempdir_to_not_existing_dir():
+    tempdir.make()
+
+    file1 = 'file1'
+    file2 = 'file2'
+    open(os.path.join(tempdir.path, file1), 'w').close()
+    open(os.path.join(tempdir.path, file2), 'w').close()
+
+    dst_dir = os.path.abspath('dst_dir')
+
+    tempdir.copy_files([file1, file2], dst_dir)
+
+    assert os.path.isfile(os.path.join(dst_dir, file1))
+    assert os.path.isfile(os.path.join(dst_dir, file2))
+
+    shutil.rmtree(dst_dir)
+    tempdir.remove()
+
+
+def test_tempdir_copy_files_copies_files_from_tempdir_to_not_existing_path():
+    tempdir.make()
+
+    file1 = 'file1'
+    file2 = 'file2'
+    open(os.path.join(tempdir.path, file1), 'w').close()
+    open(os.path.join(tempdir.path, file2), 'w').close()
+
+    dst_dir_parent = os.path.abspath('dst_dir_parent')
+    dst_dir = os.path.join(dst_dir_parent, 'dst_dir')
+
+    tempdir.copy_files([file1, file2], dst_dir)
+
+    assert os.path.isfile(os.path.join(dst_dir, file1))
+    assert os.path.isfile(os.path.join(dst_dir, file2))
+
+    shutil.rmtree(dst_dir_parent)
     tempdir.remove()
