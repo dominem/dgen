aea65c3867c42e40b7b6e41087f2602c9db2785a
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2018-08-14 14:04:52 +0200

    Rename one of tempdir tests

--- tests/test_tempdir.py
+++ tests/test_tempdir.py
@@ -9,7 +9,7 @@ def test_tempdir_make_creates_empty_temp_dir():
     tempdir.remove()
 
 
-def test_tempdir_make_removes_existing_temp_dir_and_creates_new():
+def test_tempdir_make_removes_existing_temp_dir_with_content_and_creates_new_one():
     tempdir.make()
     os.mkdir(os.path.join(tempdir.path, 'aDir'))
     assert os.listdir(tempdir.path)         # assert is-not-empty
