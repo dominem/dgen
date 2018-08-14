f682581913cc33744dcd053f6d4d0ef62745b5c1
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2018-08-14 18:15:46 +0200

    Refactor templates tests using unittest.TestCase

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from click.testing import CliRunner
 from dgen import cli
 
@@ -15,30 +16,28 @@ TEMPLATES = [f'{t}.html' for t in [
 ]]
 
 
-def assert_template_files_exist(dir_path):
-    for template in TEMPLATES:
-        template_path = os.path.join(dir_path, template)
-        assert os.path.isfile(template_path)
+class TestTemplates(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.runner = CliRunner()
 
+    @staticmethod
+    def assertTemplates(result, templates_dir_path):
+        assert result.exit_code == 0
+        assert result.output == f'Templates have been generated to the {templates_dir_path} directory.\n'
+        for template in TEMPLATES:
+            template_path = os.path.join(templates_dir_path, template)
+            assert os.path.isfile(template_path)
 
-def assert_templates(result, templates_dir_path):
-    assert result.exit_code == 0
-    assert result.output == f'Templates have been generated to the {templates_dir_path} directory.\n'
-    assert_template_files_exist(templates_dir_path)
+    def test_success_with_not_existing_templates_dir(self):
+        with self.runner.isolated_filesystem():
+            result = self.runner.invoke(cli.main, [COMMAND, TEMPLATES_DIR])
+            templates_dir_path = os.path.abspath(TEMPLATES_DIR)
+            self.assertTemplates(result, templates_dir_path)
 
-
-def test_templates_with_not_existing_templates_dir():
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        result = runner.invoke(cli.main, [COMMAND, TEMPLATES_DIR])
-        templates_dir_path = os.path.abspath(TEMPLATES_DIR)
-        assert_templates(result, templates_dir_path)
-
-
-def test_templates_with_existing_templates_dir():
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        templates_dir_path = os.path.abspath(TEMPLATES_DIR)
-        os.mkdir(templates_dir_path)
-        result = runner.invoke(cli.main, [COMMAND, TEMPLATES_DIR])
-        assert_templates(result, templates_dir_path)
+    def test_success_with_existing_templates_dir(self):
+        with self.runner.isolated_filesystem():
+            templates_dir_path = os.path.abspath(TEMPLATES_DIR)
+            os.mkdir(templates_dir_path)
+            result = self.runner.invoke(cli.main, [COMMAND, TEMPLATES_DIR])
+            self.assertTemplates(result, templates_dir_path)
