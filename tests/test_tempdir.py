1923d91de3bb125dca3200d4f06b441e8c493e80
Dominik Magdaleński <dominik.magdalenski@gmail.com>
2018-08-14 14:40:07 +0200

    Refactor tempdir tests using classes

--- tests/test_tempdir.py
+++ tests/test_tempdir.py
@@ -3,86 +3,83 @@ import shutil
 from dgen.tempdir import tempdir
 
 
-def test_tempdir_make_creates_empty_temp_dir():
-    tempdir.make()
-    assert os.path.exists(tempdir.path)
-    assert not os.listdir(tempdir.path)     # assert is-empty
-    tempdir.remove()
+class TestMake(object):
+    def test_creates_empty_tempdir(self):
+        tempdir.make()
+        assert os.path.exists(tempdir.path)
+        assert not os.listdir(tempdir.path)     # assert is-empty
+        tempdir.remove()
+
+    def test_removes_existing_tempdir_with_its_content_and_creates_new_one(self):
+        tempdir.make()
+        os.mkdir(os.path.join(tempdir.path, 'aDir'))
+        assert os.listdir(tempdir.path)         # assert is-not-empty
+
+        tempdir.make()
+        assert not os.listdir(tempdir.path)     # assert is-empty
+        tempdir.remove()
+
+    def test_removes_existing_file_that_has_name_of_tempdir_and_creates_tempdir(self):
+        open(tempdir.dirname, 'w').close()
+        assert os.path.isfile(tempdir.path)
+        tempdir.make()
+        assert os.path.isdir(tempdir.path)
+        tempdir.remove()
 
 
-def test_tempdir_make_removes_existing_temp_dir_with_content_and_creates_new_one():
-    tempdir.make()
-    os.mkdir(os.path.join(tempdir.path, 'aDir'))
-    assert os.listdir(tempdir.path)         # assert is-not-empty
+class TestCopyFiles(object):
+    def test_copies_files_from_tempdir_to_existing_dir(self):
+        tempdir.make()
 
-    tempdir.make()
-    assert not os.listdir(tempdir.path)     # assert is-empty
-    tempdir.remove()
+        file1 = 'file1'
+        file2 = 'file2'
+        open(os.path.join(tempdir.path, file1), 'w').close()
+        open(os.path.join(tempdir.path, file2), 'w').close()
 
+        dst_dir = os.path.abspath('dst_dir')
+        os.mkdir(dst_dir)
 
-def test_tempdir_make_removes_existing_temp_file_and_creates_new_dir():
-    with open(tempdir.dirname, 'w'):
-        pass
-    assert os.path.isfile(tempdir.path)
-    tempdir.make()
-    assert os.path.isdir(tempdir.path)
-    tempdir.remove()
+        tempdir.copy_files([file1, file2], dst_dir)
 
+        assert os.path.isfile(os.path.join(dst_dir, file1))
+        assert os.path.isfile(os.path.join(dst_dir, file2))
 
-def test_tempdir_copy_files_copies_files_from_tempdir_to_existing_dir():
-    tempdir.make()
+        shutil.rmtree(dst_dir)
+        tempdir.remove()
 
-    file1 = 'file1'
-    file2 = 'file2'
-    open(os.path.join(tempdir.path, file1), 'w').close()
-    open(os.path.join(tempdir.path, file2), 'w').close()
+    def test_copies_files_from_tempdir_to_not_existing_dir(self):
+        tempdir.make()
 
-    dst_dir = os.path.abspath('dst_dir')
-    os.mkdir(dst_dir)
+        file1 = 'file1'
+        file2 = 'file2'
+        open(os.path.join(tempdir.path, file1), 'w').close()
+        open(os.path.join(tempdir.path, file2), 'w').close()
 
-    tempdir.copy_files([file1, file2], dst_dir)
+        dst_dir = os.path.abspath('dst_dir')
 
-    assert os.path.isfile(os.path.join(dst_dir, file1))
-    assert os.path.isfile(os.path.join(dst_dir, file2))
+        tempdir.copy_files([file1, file2], dst_dir)
 
-    shutil.rmtree(dst_dir)
-    tempdir.remove()
+        assert os.path.isfile(os.path.join(dst_dir, file1))
+        assert os.path.isfile(os.path.join(dst_dir, file2))
 
+        shutil.rmtree(dst_dir)
+        tempdir.remove()
 
-def test_tempdir_copy_files_copies_files_from_tempdir_to_not_existing_dir():
-    tempdir.make()
+    def test_copies_files_from_tempdir_to_not_existing_path(self):
+        tempdir.make()
 
-    file1 = 'file1'
-    file2 = 'file2'
-    open(os.path.join(tempdir.path, file1), 'w').close()
-    open(os.path.join(tempdir.path, file2), 'w').close()
+        file1 = 'file1'
+        file2 = 'file2'
+        open(os.path.join(tempdir.path, file1), 'w').close()
+        open(os.path.join(tempdir.path, file2), 'w').close()
 
-    dst_dir = os.path.abspath('dst_dir')
+        dst_dir_parent = os.path.abspath('dst_dir_parent')
+        dst_dir = os.path.join(dst_dir_parent, 'dst_dir')
 
-    tempdir.copy_files([file1, file2], dst_dir)
+        tempdir.copy_files([file1, file2], dst_dir)
 
-    assert os.path.isfile(os.path.join(dst_dir, file1))
-    assert os.path.isfile(os.path.join(dst_dir, file2))
+        assert os.path.isfile(os.path.join(dst_dir, file1))
+        assert os.path.isfile(os.path.join(dst_dir, file2))
 
-    shutil.rmtree(dst_dir)
-    tempdir.remove()
-
-
-def test_tempdir_copy_files_copies_files_from_tempdir_to_not_existing_path():
-    tempdir.make()
-
-    file1 = 'file1'
-    file2 = 'file2'
-    open(os.path.join(tempdir.path, file1), 'w').close()
-    open(os.path.join(tempdir.path, file2), 'w').close()
-
-    dst_dir_parent = os.path.abspath('dst_dir_parent')
-    dst_dir = os.path.join(dst_dir_parent, 'dst_dir')
-
-    tempdir.copy_files([file1, file2], dst_dir)
-
-    assert os.path.isfile(os.path.join(dst_dir, file1))
-    assert os.path.isfile(os.path.join(dst_dir, file2))
-
-    shutil.rmtree(dst_dir_parent)
-    tempdir.remove()
+        shutil.rmtree(dst_dir_parent)
+        tempdir.remove()
